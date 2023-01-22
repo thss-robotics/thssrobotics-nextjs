a60815ff20762147bbb10a28e528eaae36f9c4c5
Matvey <thssrobotics@gmail.com>
2023-01-22 14:47:27 -0800

    inital page

--- pages/index.js
+++ pages/index.js
@@ -11,10 +11,14 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="THSS Robotics " />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          hi this is our website
         </p>
+        <p id="offset"><code>if you have ideas for what this website should like, please tell me </code></p>
+        <p><code>I'm probably going to make a subdomain for the 'claw for humanity?' or should it just be on the main page?</code></p>
+        <p><code>is 'claw for humanity' the final name for the project? we should do a vote</code></p>
+        <p id="offset"><code>if you have any experience in webdev please let me know</code></p>
       </main>
 
       <Footer />
