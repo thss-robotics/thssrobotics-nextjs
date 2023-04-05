78a91ee211d7b6fc834e79936655459c1d826a77
Matvey <123329761+thss-robotics@users.noreply.github.com>
2023-04-04 23:10:28 -0700

    Fix Tag Issue

--- pages/_document.js
+++ pages/_document.js
@@ -9,21 +9,27 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
           <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Anonymous+Pro&display=swap" rel="stylesheet" /> 
+
+          {/* Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-L89MCQMVLG"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-L89MCQMVLG');
+              `,
+            }}
+          />
+          {/* End Google Analytics */}
         </Head>
         <body>
           <Main />
           <NextScript />
-          
-
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-L89MCQMVLG"></script>
-          <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-L89MCQMVLG');
-          </script>
-
         </body>
       </Html>
     );
