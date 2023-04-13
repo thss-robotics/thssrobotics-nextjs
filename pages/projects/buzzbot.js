aceee2c49231faec3392c09e84dbb2679a55bde9
Matvey <>
2023-04-13 07:13:29 -0700

    text

--- pages/projects/buzzbot.js
+++ pages/projects/buzzbot.js
@@ -1,8 +1,9 @@
-// pages/video.js
 import Head from 'next/head';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const VideoPage = () => {
+  const [warningVisible, setWarningVisible] = useState(false);
+
   useEffect(() => {
     const handleFullscreenChange = () => {
       if (!document.fullscreenElement) {
@@ -12,8 +13,13 @@ const VideoPage = () => {
 
     document.addEventListener('fullscreenchange', handleFullscreenChange);
 
+    const warningInterval = setInterval(() => {
+      setWarningVisible(prevVisible => !prevVisible);
+    }, 500); // Adjust the duration (in milliseconds) for the flashing effect
+
     return () => {
       document.removeEventListener('fullscreenchange', handleFullscreenChange);
+      clearInterval(warningInterval);
     };
   }, []);
 
@@ -21,8 +27,24 @@ const VideoPage = () => {
     <>
       <Head>
         <title>BUZZBOT</title>
+        <style>{`
+          .warning {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            font-size: 10rem;
+            font-weight: bold;
+            color: red;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.3);
+            display: none;
+          }
+          .warning.visible {
+            display: block;
+          }
+        `}</style>
       </Head>
-      <div style={{ height: '100vh', width: '100%', overflow: 'hidden' }}>
+      <div style={{ height: '100vh', width: '100%', overflow: 'hidden', position: 'relative' }}>
         <video
           src="/buzzbot.mov"
           controls
@@ -30,6 +52,7 @@ const VideoPage = () => {
           loop
           style={{ width: '100%', height: '100%', objectFit: 'cover' }}
         ></video>
+        <span className={`warning ${warningVisible ? 'visible' : ''}`}>IT BUZZ!</span>
       </div>
     </>
   );
