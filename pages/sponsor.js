05b1c9be36ba3306805c59a9c240ace5a2984c89
Circuit-Master <128203657+Circuit-Master@users.noreply.github.com>
2024-02-26 19:05:41 -0800

    Create sponsor.js

---
+++ pages/sponsor.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SponsorForm from '../components/SponsorForm';
+
+const SponsorPage = () => {
+  return (
+    <div>
+      <h1>Sponsor Us</h1>
+      <SponsorForm />
+    </div>
+  );
+};
+
+export default SponsorPage;
