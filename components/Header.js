65ffbdcdd7d6e0caaa9f5eb603d3dc39ca8dace7
Circuit-Master <loganhumeniuk@gmail.com>
2024-06-16 22:40:14 -0700

    did a buch of work

--- components/Header.js
+++ components/Header.js
@@ -1,61 +1,54 @@
 import React from 'react';
 import { Box, useMediaQuery, useTheme, IconButton, Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import Link from 'next/link';
+
 
 const Header = () => {
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-    const scrollToSection = (sectionId) => {
-        const element = document.getElementById(sectionId);
-        if (element) {
-            element.scrollIntoView({ behavior: 'smooth' });
-        }
-    };
-
-    return (
-        <Box
-            sx={{
-                position: 'absolute',
-                top: 10,
-                left: 10,
-                right: 10,
-                display: 'flex',
-                justifyContent: isMobile ? 'center' : 'space-between', // Adjust based on screen size
-                alignItems: 'center',
-                padding: '1rem',
-                backgroundColor: 'transparent',
-                zIndex: 1100,
-            }}
-        >
-            <img 
-                src={'logoblack.png'} 
-                alt="ThunderTech Robotics Logo"
-                style={{
-                    height: '100px',
-                    filter: 'invert(100%)',
-                    position: isMobile ? 'absolute' : 'initial', // Adjust based on screen size
-                    left: isMobile ? '50%' : 'initial',
-                    top: isMobile ? '50%' : 'initial',
-                    transform: isMobile ? 'translateX(-50%)' : 'none',
-                }}
+  return (
+    <>
+      <Box
+        sx={{
+          position: 'relative', // Changed from 'fixed' to 'relative'
+          display: 'flex',
+          justifyContent: isMobile ? 'center' : 'space-between',
+          alignItems: 'center',
+          padding: '1rem',
+          backgroundColor: 'rgb(136, 0, 255)',
+        }}
+      >
+        <Link href="/" passHref>
+          <a style={{ display: 'flex', alignItems: 'center' }}>
+            <img
+              src={'/logoblack.png'}
+              alt="ThunderTech Robotics Logo"
+              style={{
+                height: '100px',
+                cursor: 'pointer',
+              }}
             />
-            {isMobile ? (
-                <IconButton
-                    sx={{ color: 'white', position: 'absolute', right: '10px' }}
-                    aria-label="menu"
-                >
-                    <MenuIcon />
-            </IconButton>
-            ) : (
-                <Box sx={{ display: 'flex' }}>
-                    <Button sx={{ margin: '0 1rem', color: 'white', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('about-us')}>About Us</Button>
-                    <Button sx={{ margin: '0 1rem', color: 'white', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('projects')}>Projects</Button>
-                    <Button sx={{ margin: '0 1rem', color: 'white', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('sponsors')}>Sponsor Us</Button>
-                </Box>
-            )}
-        </Box>
-    );
+          </a>
+        </Link>
+        {isMobile ? (
+          <IconButton
+            sx={{ color: 'black', position: 'absolute', right: '10px' }}
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+        ) : (
+          <Box sx={{ display: 'flex' }}>
+            <Button sx={{ margin: '0 1rem', color: 'black', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('about-us')}>About Us</Button>
+            <Button sx={{ margin: '0 1rem', color: 'black', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('projects')}>Projects</Button>
+            <Button sx={{ margin: '0 1rem', color: 'black', fontFamily: '"Exo 2", sans-serif', fontSize: '1.2rem' }} onClick={() => scrollToSection('sponsors')}>Sponsor Us</Button>
+          </Box>
+        )}
+      </Box>
+    </>
+  );
 };
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -6,80 +6,85 @@ import Sponsors from '../components/Sponsors';
 import Projects from '../components/Projects';
 import OurMission from '../components/OurMission';
 
+
+
 const Home = () => {
-  return (
-    <div>
-      <Box
-        sx={{
-          position: 'relative', // Changed for positioning the text
-          top: 0,
-          left: 0,
-          right: 0,
-          height: '800px',
-         // backgroundImage: 'url(https://i.postimg.cc/g0GHz2jZ/cover.jpg)',
-          backgroundImage: 'url(https://i.postimg.cc/j5nc2RMz/20240302-163146.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          zIndex: 1000,
-          '::before': {
-            content: '""',
-            position: 'absolute',
+    return (      
+     
+
+      <div>
+        <Box
+          sx={{
+           position: 'relative', // Changed for positioning the text
             top: 0,
             left: 0,
             right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(136, 0, 255, 0.4)', // Adjusted opacity
-            zIndex: -1,
-          },
-        }}
-      >
-        <Box
-          sx={{
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            transform: 'translate(-50%, -50%)',
-            textAlign: 'center',
-            color: 'white',
-            
+            height: '800px',
+           // backgroundImage: 'url(https://i.postimg.cc/g0GHz2jZ/cover.jpg)',
+            backgroundImage: 'url(https://i.postimg.cc/j5nc2RMz/20240302-163146.jpg)',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            zIndex: 1000,
+            '::before': {
+             content: '""',
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+             bottom: 0,
+              backgroundColor: 'rgba(136, 0, 255, 0.4)', // Adjusted opacity
+              zIndex: -1,
+            },
           }}
         >
-          <Typography 
-            variant="h2" 
-            sx={{ 
-              whiteSpace: 'nowrap', // Prevents the text from wrapping
-              overflow: 'hidden', // Hides overflowed text
-              textOverflow: 'ellipsis', // Adds an ellipsis to overflowed text
-              fontFamily: '"Exo 2", sans-serif', 
-              fontWeight: 'bold',
-              fontSize: { xs: '9vw', sm: '9vw', md: '7vw', lg: '4.5rem' ,}, 
+          <Box
+            sx={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: 'translate(-50%, -50%)',
+              textAlign: 'center',
+              color: 'white',
+              
             }}
           >
-            ThunderTech Robotics
-          </Typography>
-          <Typography 
-            variant="h5" 
-            sx={{ 
-              fontFamily: '"Exo 2", sans-serif',
-              fontSize: { xs: '5vw', sm: '3vw', md: '2.5vw', lg: '1.5rem' ,}, // Responsive font size
-            }}
-          >
-            Empowering Innovation and Teamwork in Robotics Technology
-          </Typography>
+            <Typography 
+              variant="h2" 
+              sx={{ 
+                whiteSpace: 'nowrap', // Prevents the text from wrapping
+               overflow: 'hidden', // Hides overflowed text
+                textOverflow: 'ellipsis', // Adds an ellipsis to overflowed text
+                fontFamily: '"Exo 2", sans-serif', 
+                fontWeight: 'bold',
+                fontSize: { xs: '9vw', sm: '9vw', md: '7vw', lg: '4.5rem' ,}, 
+              }}
+            >
+              ThunderTech Robotics
+            </Typography>
+            <Typography 
+              variant="h5" 
+              sx={{ 
+                fontFamily: '"Exo 2", sans-serif',
+                fontSize: { xs: '5vw', sm: '3vw', md: '2.5vw', lg: '1.5rem' ,}, // Responsive font size
+              }}
+            >
+              Empowering Innovation and Teamwork in Robotics Technology
+            </Typography>
+          </Box>
+
         </Box>
 
-      </Box>
+        <AboutUs />
 
-      <AboutUs />
+        <OurMission />
 
-      <OurMission />
+        <Projects />
 
-      <Projects />
+        <Sponsors />
 
-      <Sponsors />
-
-    </div>
-  );
+      </div>  
+  
+  );  
 };
 
 export default Home;
