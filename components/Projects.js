71a0a6623e435d47a538d7c555ed1787bc135316
Circuit-Master <loganhumeniuk@gmail.com>
2026-02-23 13:34:34 -0800

    organized images

--- components/Projects.js
+++ components/Projects.js
@@ -9,35 +9,35 @@ const Projects = () => {
     {
       title: 'Topographic Holobox',
       description: "A holographic sandbox for teaching topology.",
-      image: '/images/holobox.jpg',
+      image: 'holobox.jpg',
       projectId: 'holobox',
       link: 'https://www.sd42.ca/district-highlight/secondary-students-augmented/'
     },
     {
       title: 'Claw for Humanity',
       description: 'A recycling robot designed to help with waste management and promote environmental sustainability.',
-      image: '/images/claw_for_humanity2.jpg',
+      image: 'claw_for_humanity2.jpg',
       projectId: 'CFH',
       link: 'https://youtu.be/3zYi_7Qy-Lk'
     },
     {
       title: 'FRC 2024 Practice Bot',
       description: 'A practice robot built to hone our skills and prepare for the FRC 2024 competition.',
-      image: '/images/newrobotimg.jpg',
+      image: 'newrobotimg.jpg',
       projectId: 'frc-2024-practice-robot',
       link: 'https://www.youtube.com/watch?v=8kwPYAN-gxQ'
     },
     {
       title: 'FRC Swerve Drivetrain',
       description: 'Swerve drive is a sophisticated drivetrain system where each wheel can independently rotate and drive, enabling omnidirectional movement and unmatched agility.',
-      image: '/images/swervedrive.jpg',
+      image: 'swervedrive.jpg',
       projectId: 'FRC-Swerve-Drivetrain',
       link: '/SwerveDrive'
     },
     {
       title: 'FRC 2025 Robot',
       description: 'In 2025, software issues affected rotation and acceleration, but after refinement we achieved reliable performance, and our elevator consistently scored coral and removed algae.',
-      image: '/images/swervedrive.jpg',
+      image: 'swervedrive.jpg',
       projectId: 'FRC-2025-Robot',
       link: '/FRC2025Robot'
     }
@@ -94,7 +94,7 @@ const Projects = () => {
                 <CardMedia
                   component="img"
                   height="240" 
-                  image={project.image}
+                  image={`/images/projectCovers/${project.image}`}
                   alt={project.title}
                 />
                 <CardContent sx={{ flexGrow: 1, overflow: 'auto' }}> 
